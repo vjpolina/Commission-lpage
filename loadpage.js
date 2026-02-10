07280996b3e5b683d881e1100771cb4517723450
VictorRudinciuc <rudinciuk1@gmail.com>
2026-02-10 08:36:33 +0200

    Added/loaded majority of page content through JS

--- loadpage.js
+++ loadpage.js
@@ -1,12 +1,38 @@
 const content = {
     topBarImg: "images/logo1.png",
-    topBartext: ["That__po__'s Commisions", "Submit a Form", "Portfolio"],
-    info: "<b>The perfect place to order a custom illustration!</b><br><br>Here we offer commercial and non-commercial custom designs for:<i> <ul>Posters</ul><ul>Photocards</ul><ul>Pins (Both plastic and enamel)</ul><ul>Stickers</ul><ul>And more!</ul></i><br>Click on 'Submit a form' to request your own custom design now!"
-}
+    topBartext: ["That__po__'s Commissions", "Submit a Form", "Portfolio"],
+    images: [
+        "images/img1.png",
+        "images/img2.png",
+        "images/img3.png"
+    ],
+    info: `<b>The perfect place to order a custom illustration!</b><br><br>
+           Here we offer commercial and non-commercial custom designs for:
+           <ul>
+             <li>Posters</li>
+             <li>Photocards</li>
+             <li>Pins (Both plastic and enamel)</li>
+             <li>Stickers</li>
+             <li>And more!</li>
+           </ul>
+           Click on 'Submit a form' to request your own custom design now!`
+};
 
 const textBox = document.getElementsByClassName("text_box");
 const textBoxInfo = document.createElement("a");
-
 textBoxInfo.innerHTML = content.info;
 
-textBox[0].appendChild(textBoxInfo);
+if (textBox.length > 0) {
+    textBox[0].appendChild(textBoxInfo);
+}
+
+const imgContainer = document.getElementsByClassName("img_container");
+
+if (imgContainer.length > 0) {
+    for (let i = 0; i < content.images.length; i++) {
+        const img = document.createElement("img");
+        img.src = content.images[i];
+        img.id = `layer${i + 1}`;
+        imgContainer[0].appendChild(img);
+    }
+}
