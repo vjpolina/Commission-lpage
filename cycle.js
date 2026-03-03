3da079147378cfbce4e0f83b87716ef01efa5ad3
vjpolina <voljinpolina@gmail.com>
2026-03-03 11:02:48 +0200

    Save theme state/current page locally

--- cycle.js
+++ cycle.js
@@ -1,6 +1,29 @@
 let currentState = true;
 let changeButton = document.getElementById('theme_switch');
 
+window.addEventListener("load", () => {
+
+  const theme = localStorage.getItem("theme");
+  console.log("theme: ", theme);
+
+  if (theme === 'night') {
+        document.getElementById('style').href = "stylenight.css";
+        document.getElementById('theme_state').src='images/sunn.png';
+        document.getElementById('insta').src='images/instagram.png';
+        document.getElementById('tiktok').src='images/tik-tok.png';
+        document.getElementById('twit').src='images/twitter.png';
+        document.getElementById('logo').src='images/logo2.png';
+
+  } else {
+    document.getElementById('style').href = "styleday.css";
+        document.getElementById('theme_state').src='images/moon.png';
+        document.getElementById('insta').src='images/instagram_day.png';
+        document.getElementById('tiktok').src='images/tik-tok_day.png';
+        document.getElementById('twit').src='images/twitter_day.png';
+        document.getElementById('logo').src='images/logo1.png';
+  }
+});
+
 function switchTheme() {
     if (currentState) {
         document.getElementById('style').href = "stylenight.css";
@@ -9,6 +32,8 @@ function switchTheme() {
         document.getElementById('tiktok').src='images/tik-tok.png';
         document.getElementById('twit').src='images/twitter.png';
         document.getElementById('logo').src='images/logo2.png';
+
+        localStorage.setItem("theme", "night");
     } 
     else{
         document.getElementById('style').href = "styleday.css";
@@ -17,6 +42,8 @@ function switchTheme() {
         document.getElementById('tiktok').src='images/tik-tok_day.png';
         document.getElementById('twit').src='images/twitter_day.png';
         document.getElementById('logo').src='images/logo1.png';
+
+        localStorage.setItem("theme", "day");
     }
     currentState = !currentState;
 }
