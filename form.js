520e9f1eeeb7d31309cd2ccd63f9d4676fa28dc7
vjpolina <voljinpolina@gmail.com>
2026-03-17 08:06:31 +0200

    Fixed form section bugs

--- form.js
+++ form.js
@@ -16,10 +16,33 @@ window.addEventListener("load", () => {
     }   
 });
 
-function loadForm() {
-    textBoxInfo.innerHTML = content.formText;
+
+function loadForm() {textBoxInfo.innerHTML = content.formText;
+
+    const injectedForm = textBoxInfo.querySelector("form");
+    if (injectedForm) {
+        const emailInput = injectedForm.querySelector("#email");
+        const displayEmail = document.createElement("p");
+        if (emailInput) {
+            emailInput.addEventListener("input", (event) => {
+                console.log(event.target.value);
+                if (event.target.value === "") {
+                    displayEmail.textContent = " ";
+                } else {
+                displayEmail.textContent = event.target.value + " is making a commission...";
+                textBoxInfo.appendChild(displayEmail);}
+            });
+        }
+        injectedForm.addEventListener("submit", function(event) {
+            event.preventDefault();
+            injectedForm.reset();
+            textBoxInfo.removeChild(displayEmail);
+        });
+    }
+
     localStorage.setItem("openPage", "form");
 }
+
 
 function loadHome() {
     textBoxInfo.innerHTML = content.info;
